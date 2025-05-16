61e666cb1626ea263b93d30e8f2511d3afc5fc24
abdullahwaqassaad <fgg09828@gmail.com>
2025-05-16 20:41:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if submit_button:
 # Filters
 selected_symptom = st.selectbox("Select Symptom", df['symptoms'].unique())
 age_range = st.slider("Select Age Range", min_value=int(df['age'].min()), max_value=int(df['age'].max()), value=(1, 120))
-selected_gender = st.selectbox("Select Gender", ['All'] + list(df['gender'].unique()))
+selected_gender = st.selectbox("Select Gender", ['Select'] + list(df['gender'].unique()))
 
 # Apply filters
 filtered_df = df[df['symptoms'] == selected_symptom]
